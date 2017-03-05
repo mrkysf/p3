0841d442ae435d9a0e8a98d641b125de62315be4
mrkysf <markyousef23@gmail.com>
2017-03-04 22:37:31 -0500

    Update MultiThreadedServer.java

--- MultiThreadedServer.java
+++ MultiThreadedServer.java
@@ -5,7 +5,7 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.HashSet;
 
-public class MultiThreadedServer 
+public class ChattyChatChatServer 
 {
 	private static HashSet<String> names = new HashSet<String>();
 
@@ -13,16 +13,44 @@ public class MultiThreadedServer
 
 	private static ServerSocket server;
 
+	private static int numThreads = 100; 
+
+	private static final ThreadedSocket[] threads = new ThreadedSocket[numThreads];
+
 	public static void main(String[] args) throws Exception 
 	{
-		try {
-			server = new ServerSocket(9129);
+		try
+		{
+			//			if(args.length < 1)
+			//			{
+			//				System.out.println("No port number entered. Now exiting.");
+			//				System.exit(-1);
+			//			}
+			//			
+			//			int portNumber = Integer.parseInt(args[0]);
+			int portNumber = 7777;
+
+			server = new ServerSocket(portNumber);
 
 			System.out.println("Server is running.");
 			while (true) 
 			{
+				int emptyThreadIndex = 0;
+				boolean foundEmptyThread = false;
 				Socket socket = server.accept();
-				new ThreadedSocket(socket).start();
+				for(int i=0; i < numThreads; i++)
+				{
+					//.isAlive()
+					if (threads[i]==null && !foundEmptyThread)
+					{
+						emptyThreadIndex = i;
+						foundEmptyThread = true;
+					}
+				}
+				if(foundEmptyThread)
+					(threads[emptyThreadIndex] = new ThreadedSocket(socket)).start();
+				else //reach maxed num threads
+					socket.close();	
 			}
 
 		} 
@@ -30,9 +58,14 @@ public class MultiThreadedServer
 		{ 
 			e.printStackTrace();
 		}	
+		finally
+		{
+			server.close();
+		}
 	} 
 
-	 private static class ThreadedSocket extends Thread {
+	private static class ThreadedSocket extends Thread 
+	{
 		private String name;
 		private Socket socket;
 		ObjectInputStream input; 
@@ -43,13 +76,6 @@ public class MultiThreadedServer
 			this.socket = socket;
 		}
 
-		/**
-		 * Services this thread's client by repeatedly requesting a
-		 * screen name until a unique one has been submitted, then
-		 * acknowledges the name and registers the output stream for
-		 * the client in a global set, then repeatedly gets inputs and
-		 * broadcasts them.
-		 */
 		public void run() 
 		{
 			boolean runThread = true;
@@ -58,42 +84,20 @@ public class MultiThreadedServer
 
 				output = new ObjectOutputStream(socket.getOutputStream());
 
-				// Request a name from this client.  Keep requesting until
-				// a name is submitted that is not already used.  Note that
-				// checking for the existence of a name and adding the name
-				// must be done while locking the set of names.
-				while (runThread) 
-				{
+				
 					output.writeObject("Enter Your Nickname:");
 					name = (String) input.readObject();
-					//                    input.close();
-					//                    output.close();
+					
 					if (name == null) 
 					{
 						name = "anonymous";
 					}
-					synchronized (names) 
-					{
-						if (!names.contains(name)) 
-						{
 							names.add(name);
-							break;
-						}
-					}
-				}
+					
 
-				if (name == null) 
-				{
-					name = "anonymous";
-				}
-				// Now that a successful name has been chosen, add the
-				// socket's print writer to the set of all writers so
-				// this client can receive broadcast messages.
-				output.writeObject("Your username is: " + name);
+				output.writeObject("Your nickname is: " + name);
 				OutputStreams.add(output);
 
-				// Accept messages from this client and broadcast them.
-				// Ignore other clients that cannot be broadcasted to.
 				while (runThread) 
 				{
 					String message = (String) input.readObject();
@@ -102,16 +106,59 @@ public class MultiThreadedServer
 						return;
 					}
 					else if(message.startsWith("/quit")) 
-						{
-							runThread = false;
-						};
-			        
-					for (ObjectOutputStream objectOutputStream : OutputStreams) 
 					{
-						objectOutputStream.writeObject(name + ": " + message);
+						runThread = false;
 					}
-				}
-			} 
+					else if(message.startsWith("/nick")) 
+					{
+						String[] words = message.split("\\s");
+						assert( words.length > 1);
+						String newName = words[1];
+						newName.trim();
+						//assert has no spaces
+						name = newName;
+					}
+					else if (message.startsWith("/dm")) 
+					{
+						String[] words = message.split("\\s");
+						assert( words.length > 2);
+						String to = words[1];
+						String msg = null;
+						for(int i = 1; i < words.length; i++)
+						{  
+							msg += words[1];
+						}
+						assert(msg.length() > 0);
+						//this.socket.name.equals(to);
+
+						if (words.length > 1 && msg != null) 
+						{
+							if (!msg.isEmpty()) 
+							{
+								synchronized (this) 
+								{
+									for (int i = 0; i < numThreads; i++) 
+									{
+										if (threads[i] != null && threads[i] != this
+												&& threads[i].name != null
+												&& threads[i].name.equals(to))
+										{
+											threads[i].output.writeObject(name + ": " + msg);
+											this.output.writeObject(name + ": " + msg);
+										}
+									}
+								}
+							}
+						}
+					}
+					else
+					{
+						for (ObjectOutputStream objectOutputStream : OutputStreams) 
+							objectOutputStream.writeObject(name + ": " + message);
+					}
+				} 
+
+			}
 			catch (Exception e) 
 			{
 				e.printStackTrace();
@@ -120,18 +167,18 @@ public class MultiThreadedServer
 			{
 				try 
 				{
-				if (name != null) 
-				{
-					names.remove(name);
-				}
-				if (output != null) 
-				{
-					OutputStreams.remove(output);
-				}
-                  input.close();
-                  output.close();
-				  socket.close();
-				  System.out.println("Session has ended."); 
+					if (name != null) 
+					{
+						names.remove(name);
+					}
+					if (output != null) 
+					{
+						OutputStreams.remove(output);
+					}
+					input.close();
+					output.close();
+					socket.close();
+					System.out.println("Session has ended."); 
 				} 
 				catch (IOException e) 
 				{
